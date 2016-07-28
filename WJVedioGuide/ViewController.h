751aef07e061312ff83e6abb92a75f9f5d3bcbfb
王钧 <wangjun@wangjundemac.local>
2016-07-28 16:54:20 +0800

    Initial Commit

---
+++ WJVedioGuide/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WJVedioGuide
+//
+//  Created by 王钧 on 16/7/28.
+//  Copyright © 2016年 王钧. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WJVedioGuide/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WJVedioGuide
+//
+//  Created by 王钧 on 16/7/28.
+//  Copyright © 2016年 王钧. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
